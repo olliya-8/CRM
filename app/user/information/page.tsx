ec19cd3d3d3cdcc3f5d948103ee412672b2251f8
olliya-11 <olliya-11>
2026-01-20 21:03:28 +0500

    Information page error resolved

---
+++ app/user/information/page.tsx
@@ -0,0 +1,11 @@
+
+import React from "react"
+
+export default function InformationPage() {
+  return (
+    <div>
+      <h1>User Information</h1>
+      {/* Add your actual content here */}
+    </div>
+  )
+}
